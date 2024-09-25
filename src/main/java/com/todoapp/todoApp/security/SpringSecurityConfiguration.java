b8700b016dce09a7ac84003682cec4e576b2a976
Subhradip <subhradipdas6969@gmail.com>
2024-09-25 10:50:04 +0530

    h2-console connect

--- src/main/java/com/todoapp/todoApp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/todoapp/todoApp/security/SpringSecurityConfiguration.java
@@ -4,11 +4,14 @@ import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -36,5 +39,15 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        return http.build();
     }
 }
